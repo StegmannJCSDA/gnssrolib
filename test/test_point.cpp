86f1cc6b9dda451fdf9cf4bc373f9ae70296fc33
StegmannJCSDA <stegmann@ucar.edu>
2021-11-17 21:27:49 -0500

    Representing point class as a ring.

--- test/test_point.cpp
+++ test/test_point.cpp
@@ -5,18 +5,15 @@ int main()
 {
   try
   {
+    using namespace gnssro;
     //  Constructing various points:
-    gnssro::Point<double,2> p1;
-	gnssro::Point<double,2> p2(444.);
-	gnssro::Point<double,2> p3(p2);
-	gnssro::Point<double,2> p4;
-    p2 += p3;
-	p3 *= 2.;
-	p4 = p2;
-	//const gnssro::Point<double> p5 = p2 + p3;
-	std::cout << p2 << std::endl;
-	std::cout << p2[0] << std::endl;
-	std::cout << norm(p2) << std::endl;
+    point<double,3> p1;
+    point<double,3> p2(2.0);
+    p1 = p2 + p2;
+    point<double,3> p4;
+    p4 = 2.0*p2;
+    std::cout << p2 << std::endl;
+    const point<double,3> p5 = p2 - p1;
   }
   catch( int ex )
   {
